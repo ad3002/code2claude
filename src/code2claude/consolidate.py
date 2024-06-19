475d0537bc6f4b2f6a7914b0a3ac350e523fa749
Aleksey Komissarov <ad3002@gmail.com>
2024-06-19 11:30:06 +0300

    add raw format

--- src/code2claude/consolidate.py
+++ src/code2claude/consolidate.py
@@ -23,7 +23,7 @@ def extract_code(file_path):
         code = file.read()
     return code
 
-def consolidate_code(code_files):
+def consolidate_code_xml(code_files):
     consolidated_code = ""
     for file_path in code_files:
         code = extract_code(file_path)
@@ -31,19 +31,35 @@ def consolidate_code(code_files):
         consolidated_code += instruction
     return consolidated_code
 
+def consolidate_code_raw(code_files):
+    consolidated_code = ""
+    for file_path in code_files:
+        code = extract_code(file_path)
+        instruction = f"### {file_path}\n{code}\n ### end of file {file_path}\n\n"
+        consolidated_code += instruction
+    return consolidated_code
+
 def main():
     parser = argparse.ArgumentParser(description="Code Consolidator")
     parser.add_argument("-r", "--repo_path", help="Path to the code repository", required=True)
     parser.add_argument("-o", "--output_file", default="consolidated_code.code", help="Output file (default: consolidated_code.code)")
+    parser.add_argument("-f", "--format", default="xml", help="Output format xml, raw (default: xml)")
     parser.add_argument("-e", "--extensions", nargs="+", default=[".py"], help="File extensions to include (default: .py)")
     args = parser.parse_args()
 
     repo_path = args.repo_path
     extensions = args.extensions
     output_file = args.output_file
+    format = args.format
+
+    if not format in ["xml", "raw"]:
+        raise ValueError("Invalid format. Choose from xml, raw")
 
     code_files = traverse_repository(repo_path, extensions)
-    consolidated_code = consolidate_code(code_files)
+    if format == "xml":
+        consolidated_code = consolidate_code_xml(code_files)
+    elif format == "raw":
+        consolidated_code = consolidate_code_raw(code_files)
     
     with open(output_file, "w") as file:
         file.write(consolidated_code)
